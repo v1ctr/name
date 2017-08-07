2a4997bf4baef140b3b1f2466201096b7e01c5ea
5vu <ruby111401>
2017-08-07 15:32:11 +0200

    optimized message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { DBLoggedIn, DBReady } from './db';
 import { MeComponent } from './me/me.component';
-import { MessagesComponent } from './messages/messages.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/signup', pathMatch: 'full', resolve: { db: DBReady }}, // redirect to signup page
   { path: 'signup', component: SignupComponent, resolve: { db: DBReady } }, // will activate the route after the db is ready
-  { path: 'messages', component: MessagesComponent, resolve: { db: DBReady } }, // will activate the route after the db is ready
   { path: 'signup/me', component: MeComponent, canActivate: [DBLoggedIn] }, // will prevent none logged in users from accessing it
   { path: 'chats', loadChildren: './chats/chats.module#ChatsModule'} // Using loadChildren to async load the route
 ];
