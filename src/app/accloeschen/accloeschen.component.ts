d5ce4d26047ef739036348ad643075cea440548f
5vu <ruby111401>
2017-08-17 11:18:30 +0200

    acc delete

--- src/app/accloeschen/accloeschen.component.ts
+++ src/app/accloeschen/accloeschen.component.ts
@@ -11,18 +11,34 @@ import { AuthService } from '../auth.service';
 export class AccloeschenComponent implements OnInit {
 
   me: model.User;
+  bewerber: model.Bewerber;
+  unternehmen: model.Unternehmen;
 
   constructor(private router: Router, public authService : AuthService) {
     if (db.User.me) {
       this.router.navigate(['/accloeschen']);
+      this.me = db.User.me;
+      this.bewerber = new db.Bewerber();
+      this.unternehmen = new db.Unternehmen();
     }
   }
 
   ngOnInit() {
-    this.me = db.User.me;
   }
 
   delete() {
+    if (this.me.iscomp) {
+      db.Unternehmen.find().equal('userid', db.User.me.id).singleResult((unternehmen) => {
+        db.Stellenangebot.find().equal('unternehmen', db.Unternehmen.id).resultList((stellenangebote) => {
+
+        });
+        unternehmen.delete();
+      });
+    } else {
+      db.Bewerber.find().equal('userid', db.User.me.id).singleResult((bewerber) => {
+          bewerber.delete();
+      });
+    }
     db.User.me.delete();
     db.User.logout().then(() => {
       this.authService.isLoginSubject.next(false);
