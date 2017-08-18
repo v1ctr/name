0c256494c2daf9d31bcf5f10074bb9c1e0f488a9
Philipp3010 <5neuhaeu@informatik.uni-hamburg.de>
2017-08-18 12:15:20 +0200

    an der UserData Generation rumgepfuscht

--- src/app/generateUserData/generateUserData.component.ts
+++ src/app/generateUserData/generateUserData.component.ts
@@ -10,7 +10,8 @@ export class GenerateUserDataComponent {
     logs: string[] = [];
 
     constructor() {
-        let promise;
+        let promise1 = Promise.resolve(db.User.load(db.User.me.id));
+        let promise2 = Promise.resolve(db.User.load(db.User.me.id));
         for (let i = 3; i <= 4; i++) {
             const bewerber = new db.User({
                 username: 'bewerber' + i + '@swijo.com',
@@ -24,20 +25,16 @@ export class GenerateUserDataComponent {
                 iscomp: true,
                 isConfigCompleted: false,
             });
-            if (promise) {
-                promise.then(() => {
-                    this.logs.push('Registering bewerber ' + bewerber.username + '.');
-                    promise = db.User.register(bewerber, 'passwort', -1);
-                }, (error) => {
-                    this.logs.push(error.message);
-                });
-            } else {
+            promise1.then(() => {
                 this.logs.push('Registering bewerber ' + bewerber.username + '.');
-                promise = db.User.register(bewerber, 'passwort', -1);
-            }
-            promise.then(() => {
+                promise2 = db.User.register(bewerber, 'passwort', -1);
+            }, (error) => {
+                this.logs.push(error.message);
+            });
+
+            promise2.then(() => {
                 this.logs.push('Registering unternehmen ' + unternehmen.username + '.');
-                promise = db.User.register(unternehmen, 'passwort', -1);
+                promise1 = db.User.register(unternehmen, 'passwort', -1);
             }, (error) => {
                 this.logs.push(error.message);
             });
