9cc4a6d5756bf2e3c80af72ae0eb178c3addd947
Philipp3010 <5neuhaeu@informatik.uni-hamburg.de>
2017-08-19 16:57:19 +0200

    fixed broken links

--- src/app/login/newPassword/newPassword.component.ts
+++ src/app/login/newPassword/newPassword.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {db} from 'baqend';
-import {getRedirectPath} from "../db";
+import {getRedirectPath} from '../../db';
 
 @Component({
     selector: 'app-new-password',
