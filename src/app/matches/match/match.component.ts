161396190e26c4074edf6d6335fec170b25ed751
Philipp3010 <5neuhaeu@informatik.uni-hamburg.de>
2017-08-24 11:39:12 +0200

    laden des Unternehmens im Match gefixt

--- src/app/matches/match/match.component.ts
+++ src/app/matches/match/match.component.ts
@@ -9,15 +9,20 @@ import {LoggerService} from '../../logging/logger.service';
 export class MatchComponent implements OnInit {
 
     public match: model.Match;
+    public unternehmen: model.Unternehmen;
 
     constructor(private route: ActivatedRoute, private logService: LoggerService) {
     }
 
     ngOnInit() {
         const key = this.route.snapshot.params['key'];
-        db.Match.load(key, {depth: 2}).then((match) => {
+        db.Match.load(key, {depth: 1}).then((match) => {
             if (match) {
                 this.match = match;
+                db.Unternehmen.load(match.angebot.unternehmen.id).then((unternehmen) => {
+                        this.unternehmen = unternehmen;
+                    }
+                );
             }
         }, (error) => {
             this.logService.logError(error.message);
